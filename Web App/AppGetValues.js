a354df99f83a1d5dc33826fe34d0ab8be46fe477
Arnaud-Patra <arnaud.patra@etu.emse.fr>
2019-01-20 16:26:12 +0100

    get axio asynchrone

--- Web App/AppGetValues.js
+++ Web App/AppGetValues.js
@@ -1,6 +1,5 @@
 var Value = new Vue({
 
-  el:'#appGetValue',
 
   data: {
     info:[],
@@ -13,12 +12,53 @@ var Value = new Vue({
 
 
   mounted () {
-    axios
-    .get('https://faircorp-arnaud-patra.cleverapps.io/api/lights')
-    .then(response => (Value.info = response.data))
+    /*axios
+    .get("http://localhost:8080/fuseki/TestCPS2/query?query=SELECT+*+WHERE+%7B+++%3Fsub+%3Fpred+%3Fobj+.+%7D+")
+    .then(response => (this.info = response.data))
     .catch(error => console.log(error))
+*/
+    this.fetchTestData();
+    this.getData();
+/*
+    axios
+    .get('https://faircorp-arnaud-patra.cleverapps.io/api/rooms/-12')
+    .then(response => {
+      this.info = response.data;
+      this.time = response.data.floor;
+      this.x = response.data.name;
+      this.y = response.data.id;
+    })
+    .catch(error => console.log(error))
+    */
   },
 
+  methods: {
+      fetchTestData: function () {
+          console.log("coucou");
+      },
+      getData : function(){
+        /*axios
+        .get("http://localhost:8080/fuseki/TestCPS2/query?query=SELECT+*+WHERE+%7B+++%3Fsub+%3Fpred+%3Fobj+.+%7D+")
+        .then(response => (this.info = response.data))
+        .catch(error => console.log(error))
+        */
+        return axios
+        .get('https://faircorp-arnaud-patra.cleverapps.io/api/rooms/-12')
+        .then(response => {
+          this.info = response.data;
+          console.log("info = "+this.info.id);
+          return this.info;
+/*
+          this.x = this.info.name;
+          this.y = this.info.id;
+          console.log("this.x = "+this.x);
+*/
+        })
+        .catch(error => console.log(error))
+        //this.info = 'Yo'; //Test
 
+
+      }
+  }
 
 });
---
+++ Web App/test.js
@@ -0,0 +1,16 @@
+var test = new Vue({
+
+    data: {
+        test_data: []
+    },
+
+    mounted: function () {
+        this.fetchTestData();
+    },
+
+    methods: {
+        fetchTestData: function () {
+          //  console.log("coucou");
+        }
+    }
+});
