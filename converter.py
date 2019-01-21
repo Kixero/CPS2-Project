7f26246f4ca8d7a1144ac20100e6e57ebd9b375f
Anthony Meranger <anthony.meranger@gmail.com>
2019-01-21 16:26:32 +0100

    Final commit

--- converter.py
+++ converter.py
@@ -33,7 +33,7 @@ def on_publish(client,userdata,result):
     pass
 
 def to_lat_long(posX, posY):
-    #Paramètres
+    #Parametres
     a1 = -0.42 # D1 : y = a1 x + b1
     a2 = 2.7 # D2 : y = a2 x + b2
     b1 = 5895038.9
@@ -62,15 +62,15 @@ def get_formated_time():
 
 def send_update_request():
     #url_info = 'http://192.168.42.253/api/v2.0.0/status' 
-    url_info = 'http://localhost:8080/api/location'
+    url_info = 'http://faircorp-anthony-meranger.cleverapps.io/api/location'
 
     nbre = re.sub(r"\D", "",str(datetime.datetime.now()))
 
     posX, posY = get_position(url_info)
-    posX, posY = 11.3, 19.4
+    # posX, posY = 11.3, 19.4
     lat, long = to_lat_long(posX, posY)
 
-    stringQuery = '''  BASE     <http://localhost:3030>
+    stringQuery = '''  BASE     <http://www.example.org>
                 PREFIX rdf:     <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
                 PREFIX rdfs:    <http://www.w3.org/2000/01/rdf-schema#> 
                 PREFIX xsd:     <http://www.w3.org/2001/XMLSchema#> 
@@ -150,7 +150,8 @@ def on_message(client, userdata, message):
         or message.topic == 'emse/fayol/Mobile1/CPS2/test/metrics/TEMP'):
         Temperature = message.payload
         if Temperature > 55:
-            go_home()
+            #go_home()
+            pass
 
     elif (message.topic == 'emse/fayol/Mobile1/sensors/d1d7e6d4-db84-4a7c-a5e0-5bbddc9f130b/metrics/HMDT'
         or message.topic == 'emse/fayol/Mobile1/CPS2/test/metrics/HMDT'):
@@ -177,9 +178,9 @@ def go_home():
 
     try:
         urllib2.urlopen(delete_request)
-    except Exception, e:
-        print(e.code)
-        print(e.read())
+    except urllib2.URLError:
+        print('Waiting for MiR connection')
+        time.sleep(10)
 
     #TODO fill what data do we have to give to the mission_queue (ie the mission id)
     data = {}
@@ -190,12 +191,34 @@ def go_home():
 
     try:
         urllib2.urlopen(go_home_request)
-    except Exception, e:
-        print(e.code)
-        print(e.read())
+    except urllib2.URLError:
+        print('Waiting for MiR connection')
+        time.sleep(10)
 
 
+def get_position(urllink):
+    try:
+        url = urllib2.urlopen(urllink)
+        print('Connection to mir100')
+        response = json.loads(url.read())
+        url.close()
 
+        for key1, value1 in response.items():
+            if (key1 == 'x'):
+                posX = value1
+            elif (key1 == 'y'):
+                posY = value1
+
+        print('MiR connection closed')
+
+    except urllib2.URLError:
+        print('Waiting for MiR connection')
+        time.sleep(10)
+
+    return posX, posY
+
+
+"""
 def get_position(urllink):
     try:
         url = urllib2.urlopen(urllink)
@@ -220,10 +243,13 @@ def get_position(urllink):
 
     return posX, posY
 
+"""
+
 
 def connectMQQT():
-    #broker_address= "193.49.165.40"
-    #broker_port = 1883    
+    #brokeraddress= "193.49.165.40"
+    #brokerport = 1883    
+    #brokeraddress = '10.103.1.149'
     brokeraddress = 'localhost'
     brokerport = 1884
 
@@ -250,7 +276,7 @@ def main():
             time.sleep(10)
         while Connected:
             send_update_request()
-            time.sleep(3)
+            time.sleep(5)
     disconnectMQTT(client)
         
         
